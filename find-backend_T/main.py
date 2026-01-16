553fe8493ae989e249612371278e3ef993b6ea17
asd1702 <gyeonghyeongib@gmail.com>
2026-01-16 23:35:29 +0900

    feat:CORS 규칙 추가

--- find-backend_T/main.py
+++ find-backend_T/main.py
@@ -34,7 +34,14 @@ app = FastAPI()
 # CORS 설정 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173",
+        "https://find-frontend-kappa.vercel.app",
+        "https://find-frontend-kappa.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
